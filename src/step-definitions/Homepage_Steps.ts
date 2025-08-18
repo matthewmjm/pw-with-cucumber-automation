833d83a8099142d1864c25658fb4d952acd8d942
matthewmjm <matthewmjm@yahoo.com>
2025-08-18 10:46:00 -0600

    imports and lets for the homepage steps

--- src/step-definitions/Homepage_Steps.ts
+++ src/step-definitions/Homepage_Steps.ts
@@ -1,4 +1,9 @@
 import { Given, When, Then } from "@cucumber/cucumber";
+import { Browser, Page } from "playwright";
+
+let browser: Browser; //Represents the browser instance (e.g., Chrome, Firefox) opened by Playwright
+let context: any; //Represents a browser context (a separate browsing session); Each context has its own cookies, cache, and storage
+let page: Page; //Represents a single webpage within a context
 
 Given("I navigate to the webdriveruniversity homepage", async () => {
 	console.log("Step 1");
