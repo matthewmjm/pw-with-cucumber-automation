dfb7a6dd3832277dfdf82cf55efae1b6697baa06
matthewmjm <matthewmjm@yahoo.com>
2025-08-19 12:10:01 -0600

    write remaining tests for the Contact Us steps

--- src/step-definitions/ContactUs_Steps.ts
+++ src/step-definitions/ContactUs_Steps.ts
@@ -1,7 +1,42 @@
-import { When } from "@cucumber/cucumber";
+import { Then, When } from "@cucumber/cucumber";
 import { pageFixture } from "./hooks/browserContextFixture";
+import { expect } from "@playwright/test";
 
 When("I type a first name", async () => {
 	await pageFixture.page.getByPlaceholder("First Name").fill("Joe");
+});
+
+When("I type a last name", async () => {
+	await pageFixture.page.getByPlaceholder("Last Name").fill("Smith");
+});
+
+When("I enter an email", async () => {
+	await pageFixture.page.getByPlaceholder("Email Address").fill("joe.smith@sol.com");
+});
+
+When("I type a comment", async () => {
+	await pageFixture.page.getByRole("textbox", { name: "Comments" }).fill("I am Joe Smith and this is my comment.");
+});
+
+When("I click on the submit button", async () => {
+	// await pageFixture.page.getByRole("button", { name: "SUBMIT" }).click();
+
+	// wait for the button to load
+	await pageFixture.page.waitForSelector("input[value='SUBMIT']");
+	// once loaded, click on the button
+	await pageFixture.page.click("input[value='SUBMIT']");
+});
+
+Then("I should be presented with a successful contact us submission message", async () => {
+	await pageFixture.page.waitForSelector("#contact_reply h1", { timeout: 60000 });
+
+	// get the text from the h1 element
+	const text = await pageFixture.page.innerText("#contact_reply h1");
+
+	// use Playwright assertion
+	expect(text).toBe("Thank You for your Message!");
+
+	// await pageFixture.page.getByRole("heading", { name: "Thank You for your Message!" });
 	// await pageFixture.page.pause();
+	// locator('#contact_reply')
 });
