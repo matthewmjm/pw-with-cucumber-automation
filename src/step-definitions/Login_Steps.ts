1f2277a7daa214ea4d8b92cefd3045ba1fd484d2
matthewmjm <matthewmjm@yahoo.com>
2025-08-23 17:05:29 -0600

    remove unneeded code

--- src/step-definitions/Login_Steps.ts
+++ src/step-definitions/Login_Steps.ts
@@ -14,23 +14,19 @@ When("I type a username {word}", async (username: string) => {
 
 When("I type a password {word}", async (password: string) => {
 	await pageFixture.page.getByPlaceholder("Password").fill(password);
-	//await pageFixture.page.waitForTimeout(2000);
 });
 
 When("I click on the login button", async () => {
 	// listener for alert
 	await pageFixture.page.on("dialog", async (alert) => {
 		alertText = alert.message();
-		//console.log("alertText: ", alertText);
 		await alert.accept();
 	});
 	const loginButton = await pageFixture.page.locator("#login-button");
 	await loginButton.hover();
 	await loginButton.click({ force: true });
-	//await pageFixture.page.waitForTimeout(2000);
 });
 
 Then("I should be presented with an alert box which contains text {string}", async (expectedAlertText: string) => {
-	// assertion
 	expect(alertText).toBe(expectedAlertText);
 });
