f2a23ffc43823045ab51492447299a0260f9957a
matthewmjm <matthewmjm@yahoo.com>
2025-08-23 10:39:23 -0600

    Logger error handling

--- src/step-definitions/Homepage_Steps.ts
+++ src/step-definitions/Homepage_Steps.ts
@@ -1,11 +1,18 @@
 import { Given, When } from "@cucumber/cucumber";
 import { pageFixture } from "./hooks/browserContextFixture";
+import logger from "../logger/logger";
 
 const url = "https://www.webdriveruniversity.com/";
 
 Given("I navigate to the webdriveruniversity homepage", async () => {
-	// Access URL
-	await pageFixture.page.goto(url);
+	try {
+		// Access URL
+		await pageFixture.page.goto(url);
+		logger.info("Accessing URL: " + url);
+		// throw new Error("Simulating an error during navigation");
+	} catch (error: any) {
+		logger.error("An error has occurred: " + url);
+	}
 });
 
 When("I click on the contact us button", async () => {
