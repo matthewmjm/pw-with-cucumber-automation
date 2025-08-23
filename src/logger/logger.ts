a1567fd3aef569616018c199f6572f523a5600a9
matthewmjm <matthewmjm@yahoo.com>
2025-08-22 19:45:29 -0600

    configure logger

---
+++ src/logger/logger.ts
@@ -0,0 +1,31 @@
+import { transports, format, createLogger } from "winston";
+import colors from "@colors/colors";
+
+import dotenv from "dotenv";
+dotenv.config({ path: "./env/.env" });
+
+// Define custom format
+const myFormat = format.printf(({ level, message, timestamp }) => {
+	let colorizedMessage = message;
+	// switch (level) {
+	// 	case "error":
+	// 		colorizedMessage = colors.red(message);
+	// 		break;
+	// 	case "warn":
+	// 		colorizedMessage = colors.yellow(message);
+	// 		break;
+	// 	case "info":
+	// 		colorizedMessage = colors.green(message);
+	// 		break;
+	// }
+	return `${timestamp} ${level}: ${colorizedMessage}`;
+});
+
+// Create logger instance
+const logger = createLogger({
+	level: process.env.LOG_LEVEL || "info",
+	format: format.combine(format.timestamp(), myFormat),
+	transports: [new transports.Console()],
+});
+
+export default logger;
