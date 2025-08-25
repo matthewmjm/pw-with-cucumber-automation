e27a83e0e714bbcdb72dbe4f345035d6a71824f7
matthewmjm <matthewmjm@yahoo.com>
2025-08-25 13:12:27 -0600

    remove playwright files

--- src/step-definitions/hooks/browserContextFixture.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.pageFixture = void 0;
-exports.pageFixture = {
-    // @ts-ignore
-    page: undefined,
-    // @ts-ignore
-    context: undefined,
-};
--- src/step-definitions/hooks/browserContextFixture.ts
+++
@@ -1,9 +0,0 @@
-import { BrowserContext, Page } from "playwright/test";
-
-export const pageFixture = {
-	// @ts-ignore
-	page: undefined as Page,
-
-	// @ts-ignore
-	context: undefined as BrowserContext,
-};
