1b2f157eeb01aa40ebf893b09512ab70d80fc795
matthewmjm <matthewmjm@yahoo.com>
2025-08-25 11:17:41 -0600

    use Arrays in TS

---
+++ TYPESCRIPT-NODEJS-FUNDAMENTALS/ts-examples/src/example-5/variable-data-types.ts
@@ -0,0 +1,10 @@
+// Part 1 - Basic Variables
+// Declare a variable with a specific type
+let message: string = "Hello, TypeScript";
+console.log("Message: ", message);
+
+// Part 2 - Arrays
+// Declare an array of numbers
+let numbers: number[] = [1, 2, 3];
+console.log("Numbers Array: ", numbers);
+console.log("Numbers Array at index 1: ", numbers[1]);
