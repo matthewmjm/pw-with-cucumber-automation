44b0581b3809130f3767d144cc8d5b4dd1fb13c1
matthewmjm <matthewmjm@yahoo.com>
2025-08-25 12:58:02 -0600

    add express

--- TYPESCRIPT-NODEJS-FUNDAMENTALS/ts-examples/src/example-12/app.ts
+++ TYPESCRIPT-NODEJS-FUNDAMENTALS/ts-examples/src/example-12/app.ts
@@ -1,13 +1,13 @@
-import express, {Request, Response} from "express";
+import express, { Request, Response } from "express";
 
 const app = express();
 
 app.get("/", (req: Request, res: Response) => {
-    res.send("hello world");
+	res.send("hello world");
 });
 
 const PORT = 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
+	console.log(`Server is running on port ${PORT}`);
 });
 //http://localhost:3000/
